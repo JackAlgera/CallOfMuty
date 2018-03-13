a42fd63d80451715164965f2088eecabc51d4eac
Thomas BERTIN <tbertin@ens2m.fr>
2018-03-13 11:54:43 +0100

    Thomas version before git was enabled (better ?)

--- src/callofmuty/CallOfMuty.java
+++ src/callofmuty/CallOfMuty.java
@@ -1,22 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package callofmuty;
 
-/**
- *
- * @author jalgera
- */
-public class CallOfMuty {
+import javax.swing.JFrame;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        //test test stestsetset
+public class CallOfMuty {
+    
+    public static void main(String[] args) throws InterruptedException {
+        GameTimer timer = new GameTimer();
+        
+        int textureSize, mapWidth, mapHeight, maxFPS;
+        long dT, minUpdateTime;
+        
+        textureSize=64;
+        mapWidth= 16;
+        mapHeight= 9;
+        String frameTitle = "Call of µty";
+        maxFPS = 60;
+        
+        minUpdateTime =(long) 1000/maxFPS;
+        
+        GamePanel game = new GamePanel(textureSize, mapWidth, mapHeight);
+        JFrame frame = createJFrame(frameTitle, game);
+        
+        timer.update();
+        
+        while(true){
+            dT = timer.update();
+            game.updateGame(dT);
+            dT =timer.getDT();
+            if (dT<minUpdateTime){
+                Thread.sleep(minUpdateTime-dT);
+            }
+            game.repaint();
+        }
     }
     
+        private static JFrame createJFrame(String frameTitle, GamePanel game){
+        JFrame frame = new JFrame();
+        frame.setTitle(frameTitle);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // stops application when frame is closed
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.add(game);
+        frame.pack();
+        frame.setLocationRelativeTo(null); // appears at the centre of the screen
+        return frame;
+    }
 }
