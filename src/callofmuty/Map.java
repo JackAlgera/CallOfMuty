2cf862da9514e489208cf61a27d9b4f7d408f812
Thomas BERTIN <tbertin@ens2m.fr>
2018-03-13 16:20:57 +0100

    Cleaner map system

--- src/callofmuty/Map.java
+++ src/callofmuty/Map.java
@@ -4,66 +4,57 @@ import java.awt.Graphics2D;
 
 public class Map{
     
-    private Tile[][] map;
-    private int mapWidth,mapHeight;
+    private int[][] map;
+    private int mapWidth,mapHeight, tileSize;
+    private static TileType grass = TileType.GRASS, rock = TileType.ROCK, dirt = TileType.ROCK;
     
-    public Map(Tile[][] map){
+    // Grass = 0 ; Rock = 1 ; Dirt = 2;
+    
+    public Map(int[][] map, int tileSize){
         this.map=map;
         mapHeight=map.length;
         mapWidth=map[0].length;
-    }
-    
-    public Map(int[][] intMap, int tileSize){
-        mapHeight=intMap.length;
-        mapWidth=intMap[0].length;
-        map = new Tile[mapWidth][mapHeight];
-        for (int i = 0 ; i<mapWidth ; i++){
-            for (int j = 0; j<mapHeight; j++){
-                switch(intMap[j][i]){
-                    case 1:
-                        map[i][j] = new Tile(i*tileSize,j*tileSize,tileSize,TileType.DIRT);
-                        break;
-                    case 2:
-                        map[i][j] = new Tile(i*tileSize,j*tileSize,tileSize,TileType.ROCK);
-                        break;
-                    default:
-                        map[i][j] = new Tile(i*tileSize,j*tileSize,tileSize,TileType.GRASS);
-                }
-                    
-            }
-        }
+        this.tileSize = tileSize;
     }
     
     public Map(int mapWidth, int mapHeight, int tileSize){
         this.mapHeight = mapHeight;
         this.mapWidth = mapWidth;
-        map = new Tile[mapWidth][mapHeight];
+        this.tileSize = tileSize;
+        map = new int[mapWidth][mapHeight];
         for (int i = 1 ; i<mapWidth-1 ; i++){
             for (int j = 1; j<mapHeight-1; j++){
-                map[i][j] = new Tile(i*tileSize,j*tileSize,tileSize,TileType.GRASS);
+                map[i][j] = 0;
             }
         }
         for (int i = 0 ; i<mapWidth ; i++){
-            map[i][0] = new Tile(i*tileSize,0,tileSize,TileType.ROCK);
-            map[i][mapHeight-1] = new Tile(i*tileSize,(mapHeight-1)*tileSize,tileSize,TileType.ROCK);
+            map[i][0] = 1;
+            map[i][mapHeight-1] = 1;
         }
         for (int j = 1 ; j<mapHeight-1 ; j++){
-            map[0][j] = new Tile(0,j*tileSize,tileSize,TileType.ROCK);
-            map[mapWidth-1][j] = new Tile((mapWidth-1)*tileSize,j*tileSize,tileSize,TileType.ROCK);
+            map[0][j] = 1;
+            map[mapWidth-1][j] = 1;
         }
     }
     
     public void draw(Graphics2D g2d){
-        int x = map.length, y = map[0].length;
-        for (int i = 0 ; i<x ; i++){
-            for (int j = 0; j<y; j++){
-                map[i][j].draw(g2d);
+        for (int i = 0 ; i<mapHeight ; i++){
+            for (int j = 0; j<mapWidth; j++){
+                switch(map[j][i]){
+                    case 1:
+                        g2d.drawImage(rock.getImage(),j*64, i*64, tileSize, tileSize, null);
+                        break;
+                    case 2:
+                        g2d.drawImage(dirt.getImage(), j*64, i*64, tileSize, tileSize, null);
+                        break;
+                    default:
+                        g2d.drawImage(grass.getImage(), j*64, i*64, tileSize, tileSize, null);
+}
             }
         }
     }
     
-    public Tile getTile(int x, int y){
-        int tileSize = map[0][0].getSize();
+    public int getTile(int x, int y){
         return map[x/tileSize][y/tileSize];
     }
     
