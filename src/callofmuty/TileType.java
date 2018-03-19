35d275ee754474d3d5a50dd50a8a5b710421278a
Thibault Laurendon <t.laurendon@gmail.com>
2018-03-19 21:31:10 +0100

    Tile generator operational but VERRY laggy

--- src/callofmuty/TileType.java
+++ src/callofmuty/TileType.java
@@ -1,27 +1,43 @@
 package callofmuty;
 
-import java.awt.Image;
-import javax.swing.ImageIcon;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public enum TileType {
     
-    GRASS("images/grass.png", true), DIRT("images/dirt.png",true), ROCK("images/rock.png",false);
+    GRASS(true,1,1), DIRT(true,1,2), ROCK(false,1,3);
     
-    private boolean isCrossable;
-    private Image image;
+    private final boolean isCrossable;
+    private int colone;
+    private int line;
     
-    TileType(String file, boolean isCrossable){
+    
+    private BufferedImage image;
+    File tileset = new File("images/Tileset.png");
+
+
+    TileType( boolean isCrossable, int colone, int line){
         this.isCrossable = isCrossable;
-        image = new ImageIcon(file).getImage();
+        this.colone=colone;
+        this.line=line;
     }
 
-    public boolean isIsCrossable() {
+    public boolean IsCrossable() {
         return isCrossable;
     }
 
-    public Image getImage() {
-        return image;
+    public BufferedImage loadAndSelectaTile(File tilesetfile, int colone, int line){
+        try {
+            image = ImageIO.read(tilesetfile);
+        } catch (IOException error) {
+            System.out.println("Error: cannot read tileset image.");        }
+        int x =32*(colone-1);
+        int y =32*(line-1);
+        return image.getSubimage(x, y, 32, 32);
     }
-    
-    
+    public BufferedImage getImage() {
+       return loadAndSelectaTile(tileset, this.colone, this.line);
+    }
 }
