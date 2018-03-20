2bde09cb0562ef6903ec9d2f58a2874dc8fce586
Thibault LAURENDON <tlaurend@ens2m.fr>
2018-03-20 10:16:12 +0100

    Creation of a new map

--- src/callofmuty/Map.java
+++ src/callofmuty/Map.java
@@ -8,8 +8,9 @@ public class Map{
     private int[][] map;
     private int mapWidth,mapHeight, textureSize;
     private static TileType grass = new TileType(true,1,1);
-    private static TileType dirt = new TileType(true,1,18);
-    private static TileType rock= new TileType(false,1,12); 
+    private static TileType dirt = new TileType(true,1,1);
+    private static TileType woodt= new TileType(false,1,12), woodb= new TileType(false,3,12), woodl= new TileType(false,2,11), woodr= new TileType(false,2,13), woodtl= new TileType(false,1,11), woodbl= new TileType(false,3,11), woodtr= new TileType(false,1,13), woodbr= new TileType(false,3,13);
+    
     
 // Grass = 0 ; Rock = 1 ; Dirt = 2;
     
@@ -33,14 +34,22 @@ public class Map{
                 map[i][j] = 0;
             }
         }
-        for (int i = 0 ; i<mapWidth ; i++){
+        for (int i = 1 ; i<mapWidth-1 ; i++){
             map[i][0] = 1;
-            map[i][mapHeight-1] = 1;
+        }
+        for (int i = 1 ; i<mapWidth-1 ; i++){
+            map[i][mapHeight-1] = 2;
         }
         for (int j = 1 ; j<mapHeight-1 ; j++){
-            map[0][j] = 1;
-            map[mapWidth-1][j] = 1;
+            map[0][j] = 3;
         }
+        for (int j = 1 ; j<mapHeight-1 ; j++){
+             map[mapWidth-1][j] = 4;
+        }
+        map[0][0] = 5;
+        map[0][mapHeight-1] = 6;
+        map[mapWidth-1][0] = 7;
+        map[mapWidth-1][mapHeight-1] = 8;        
     }
     
     public void draw(Graphics2D g2d){
@@ -48,13 +57,33 @@ public class Map{
             for (int j = 0; j<mapWidth; j++){
                 switch(map[j][i]){
                     case 1:
-                        g2d.drawImage(rock.getImage(),j*textureSize, i*textureSize, textureSize, textureSize, null);
+                        g2d.drawImage(woodt.getImage(),j*textureSize, i*textureSize, textureSize, textureSize, null);
                         break;
                     case 2:
-                        g2d.drawImage(dirt.getImage(), j*textureSize, i*textureSize, textureSize, textureSize, null);
+                        g2d.drawImage(woodb.getImage(), j*textureSize, i*textureSize, textureSize, textureSize, null);
                         break;
+                    case 3:
+                        g2d.drawImage(dirt.getImage(), j*textureSize, i*textureSize, textureSize, textureSize, null);
+                        g2d.drawImage(woodl.getImage(), j*textureSize, i*textureSize, textureSize, textureSize, null);
+                        break;
+                    case 4:
+                        g2d.drawImage(dirt.getImage(), j*textureSize, i*textureSize, textureSize, textureSize, null);
+                        g2d.drawImage(woodr.getImage(), j*textureSize, i*textureSize, textureSize, textureSize, null);
+                        break;
+                    case 5:
+                        g2d.drawImage(woodtl.getImage(), j*textureSize, i*textureSize, textureSize, textureSize, null);
+                        break;
+                    case 6:
+                        g2d.drawImage(woodbl.getImage(), j*textureSize, i*textureSize, textureSize, textureSize, null);
+                        break;
+                    case 7:                        
+                        g2d.drawImage(woodtr.getImage(), j*textureSize, i*textureSize, textureSize, textureSize, null);
+                        break;
+                    case 8:
+                        g2d.drawImage(woodbr.getImage(), j*textureSize, i*textureSize, textureSize, textureSize, null);
+                        break;                                        
                     default:
-                        g2d.drawImage(grass.getImage(), j*textureSize, i*textureSize, textureSize, textureSize, null);
+                        g2d.drawImage(dirt.getImage(), j*textureSize, i*textureSize, textureSize, textureSize, null);
 }
             }
         }
